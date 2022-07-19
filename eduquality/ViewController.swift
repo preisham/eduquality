60a08f88ce252f0068224899d5e5d1f856641ba8
Preisha Mishra <preisha1mishra@gmail.com>
2022-07-19 15:43:07 -0400

    made 6 segues, 6 VCs, and connected homepage buttons to ViewController.swift

--- eduquality/ViewController.swift
+++ eduquality/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func africa(_ sender: UIButton) {
+    }
+    
+    @IBAction func asia(_ sender: UIButton) {
+    }
+
+
+    @IBAction func austalia(_ sender: UIButton) {
+    }
+    
+
+
+    @IBAction func europe(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func nAmerica(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func sAmerica(_ sender: UIButton) {
+    }
 
 }
 
