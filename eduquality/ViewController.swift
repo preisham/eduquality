5ef7183911ca1f049aac8b5bb55979c1125514c7
Preisha Mishra <preisha1mishra@gmail.com>
2022-07-19 16:06:37 -0400

    very roughly drafted welcome page & login page; also connected the view controllers to their prospective files

--- eduquality/ViewController.swift
+++ eduquality/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+    
     }
 
     @IBAction func africa(_ sender: UIButton) {
