136836e1434980b20071b047b01ea592485c1914
Preisha Mishra <preisha1mishra@gmail.com>
2022-07-20 12:40:48 -0400

    change style of button labels for africaVC for better contrast in color (made button bigger, more rounded, and made text black)

--- eduquality/welcomeVC.swift
+++ eduquality/welcomeVC.swift
@@ -6,16 +6,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class welcomeVC: UIViewController {
 
+    @IBOutlet weak var music: UIButton!
+    var player: AVAudioPlayer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
+   
+    @IBAction func didTapButton(_ sender: Any) {
+        
+        if let player = player, player.isPlaying {
+            //stop playback
+            player.stop()
+        } else {
+            //set up player, and play
+            let urlString = Bundle.main.path(forResource: "childhood", ofType: "mp3")
+            
+            do{
+               try AVAudioSession.sharedInstance().setMode(.default)
+               try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                guard let urlString = urlString else {
+                    return
+               }
+
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                player.play()
+            
+            }
+            catch {
+                print("something went wrong")
+            }
+        }
+    
+        
+        
+    }
     /*
     // MARK: - Navigation
 
