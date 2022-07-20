afe389d57922f834fe94a9311fb64b7f447cd4d4
Preisha Mishra <preisha1mishra@gmail.com>
2022-07-20 12:15:38 -0400

    added music  playback yay

---
+++ eduquality/loginVC.swift
@@ -0,0 +1,33 @@
+//
+//  loginVC.swift
+//  eduquality
+//
+//  Created by Preisha Mishra on 7/20/22.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginTap(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ eduquality/signUpVC.swift
@@ -0,0 +1,38 @@
+//
+//  signUpVC.swift
+//  eduquality
+//
+//  Created by Preisha Mishra on 7/20/22.
+//
+
+import UIKit
+
+class signUpVC: UIViewController {
+
+    
+    @IBOutlet weak var fullName: UITextField!
+    
+    @IBOutlet weak var dateOfBirth: UIDatePicker!
+    
+    @IBOutlet weak var emailRegister: UITextField!
+    
+    @IBOutlet weak var passwordRegister: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
