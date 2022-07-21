db9d53500ef14f1e2a5b9f80c789ff61e0bc090e
Preisha Mishra <preisha1mishra@gmail.com>
2022-07-20 20:56:22 -0400

    scroll view added to GuineaVC with initial text

---
+++ eduquality/guineaVC.swift
@@ -0,0 +1,21 @@
+//
+//  guineaVC.swift
+//  eduquality
+//
+//  Created by Preisha Mishra on 7/20/22.
+//
+
+import UIKit
+
+class guineaVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        }
+        
+        
+    
+
+}
