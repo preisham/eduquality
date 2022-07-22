27a8513770eb2c7effeac7e61dc415e1ec6f3c1a
Preisha Mishra <preisha1mishra@gmail.com>
2022-07-22 01:27:32 -0400

    done with asian countries

---
+++ eduquality/nepalVC.swift
@@ -0,0 +1,29 @@
+//
+//  nepalVC.swift
+//  eduquality
+//
+//  Created by Preisha Mishra on 7/22/22.
+//
+
+import UIKit
+
+class nepalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
